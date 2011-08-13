bc4087e632bc41264cc7c97e5d9fcfc830d4ef96
Alex Silverstein <aslvrstn@gmail.com>
2011-08-13 17:18:06 +0100

    Fixing toList

--- trie.hs
+++ trie.hs
@@ -3,9 +3,12 @@ type Trie = [TrieEdge]
 data TrieEdge = TrieEdge Char Bool Trie deriving Show
 
 toList :: Trie -> [String]
-toList [] = []
-toList ((TrieEdge c w t):es) = let rest = (map (c:) (toList t))
-                               in if w then [c]:rest else rest
+toList = concatMap toList'
+    where toList' (TrieEdge c w t) = let rest = (map (c:) (toList t))
+                                     in if w then [c]:rest else rest
+
+fromList :: [String] -> Trie
+fromList = foldr insert []
 
 insert :: String -> Trie -> Trie
 insert [] t = t
