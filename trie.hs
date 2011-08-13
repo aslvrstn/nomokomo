b89954ce431198c572a608c7166611e83b13f2d9
Alex Silverstein <aslvrstn@gmail.com>
2011-08-13 16:31:02 +0100

    Use type instead of separate type for Trie

--- trie.hs
+++ trie.hs
@@ -1,11 +1,8 @@
-data Trie = Trie [TrieEdge] deriving Show
+type Trie = [TrieEdge]
 
 data TrieEdge = TrieEdge Char Bool Trie deriving Show
 
-empty :: Trie
-empty = Trie []
-
 toList :: Trie -> [String]
-toList (Trie []) = []
-toList (Trie ((TrieEdge c w t):es)) = let rest = (map (c:) (toList t))
-                                      in if w then [c]:rest else rest
+toList [] = []
+toList ((TrieEdge c w t):es) = let rest = (map (c:) (toList t))
+                               in if w then [c]:rest else rest
